fd8681d73a4cfe7c946c5ff229467271c7413f8f
xu.zhang <xu.zhang@ihandysoft.com>
2016-02-01 21:14:40 +0800

    fix bug: input method service get killed during update

--- app/src/main/java/com/ihs/inputmethod/latin/LatinIME.java
+++ app/src/main/java/com/ihs/inputmethod/latin/LatinIME.java
@@ -1,10 +1,10 @@
-package com.keyboard.rainbow.app;
+package com.ihs.inputmethod.latin;
 
 import com.ihs.inputmethod.framework.HSInputMethodService;
 
 /**
  * Created by xu.zhang on 11/3/15.
  */
-public class MyInputMethodService extends HSInputMethodService {
+public class LatinIME extends HSInputMethodService {
 
 }
