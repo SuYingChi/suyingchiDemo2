318f98628b10312b118fad8ba9f966c6636a0176
Yuanming Chen <yuanming.chen@ihandysoft.com>
2016-06-30 16:09:18 +0800

    bug fix: input letters  did not place in gif search EditText when it has focus

--- app/src/main/java/com/ihs/inputmethod/latin/LatinIME.java
+++ app/src/main/java/com/ihs/inputmethod/latin/LatinIME.java
@@ -7,11 +7,13 @@ import android.view.inputmethod.InputConnection;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.ihs.commons.notificationcenter.HSGlobalNotificationCenter;
 import com.ihs.commons.utils.HSBundle;
+import com.ihs.commons.notificationcenter.INotificationObserver;
 import com.ihs.commons.utils.HSLog;
 import com.ihs.inputmethod.api.HSInputMethod;
 import com.ihs.inputmethod.framework.HSInputMethodService;
 import com.ihs.inputmethod.framework.HSKeyboardSwitcher;
 import com.ihs.inputmethod.uimodules.KeyboardPluginManager;
+import com.ihs.inputmethod.uimodules.constants.Constants;
 import com.ihs.inputmethod.uimodules.ui.gif.common.ui.view.CustomSearchEditText;
 import com.ihs.inputmethod.uimodules.ui.gif.riffsy.control.DataManager;
 import com.ihs.inputmethod.uimodules.ui.gif.riffsy.dao.base.LanguageDao;
@@ -29,9 +31,24 @@ public class LatinIME extends HSInputMethodService {
     private static InputConnection insideConnection=null;
     private volatile boolean isOnDestroy=false;
 
+    private  INotificationObserver keyboardNotificationObserver = new INotificationObserver() {
+        @Override
+        public void onReceive(String eventName, HSBundle notificaiton) {
+            if (eventName.equals(Constants.HS_NOTIFICATION_START_INPUT_INSIDE)) {
+                CustomSearchEditText customSearchEditText = (CustomSearchEditText)notificaiton.getObject(Constants.HS_NOTIFICATION_CUSTOM_SEARCH_EDIT_TEXT);
+                onStartInputInside(customSearchEditText);
+            } else if (eventName.equals(Constants.HS_NOTIFICATION_FINISH_INPUT_INSIDE)) {
+                onFinishInputInside();
+            }
+        }
+    };
     @Override
     public void onCreate() {
         super.onCreate();
+
+        HSGlobalNotificationCenter.addObserver(Constants.HS_NOTIFICATION_START_INPUT_INSIDE,keyboardNotificationObserver);
+        HSGlobalNotificationCenter.addObserver(Constants.HS_NOTIFICATION_FINISH_INPUT_INSIDE,keyboardNotificationObserver);
+
         Fresco.initialize(this);
         AsyncThreadPools.execute(new Runnable() {
             @Override
