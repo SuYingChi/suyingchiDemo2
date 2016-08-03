707a852ef3b6190ad1dd67dcd9600bfb688033e0
xu.zhang <xu.zhang@ihandysoft.com>
2016-08-03 16:10:41 +0800

    update lib ui module

--- app/src/main/java/com/ihs/inputmethod/latin/LatinIME.java
+++ app/src/main/java/com/ihs/inputmethod/latin/LatinIME.java
@@ -1,21 +1,13 @@
 package com.ihs.inputmethod.latin;
 
 import android.content.res.Configuration;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputConnection;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.ihs.commons.notificationcenter.HSGlobalNotificationCenter;
-import com.ihs.commons.utils.HSBundle;
-import com.ihs.commons.notificationcenter.INotificationObserver;
 import com.ihs.commons.utils.HSLog;
 import com.ihs.inputmethod.api.HSInputMethod;
 import com.ihs.inputmethod.api.HSUIInputMethodService;
-import com.ihs.inputmethod.framework.HSInputMethodService;
 import com.ihs.inputmethod.framework.HSKeyboardSwitcher;
 import com.ihs.inputmethod.uimodules.KeyboardPluginManager;
-import com.ihs.inputmethod.uimodules.constants.Constants;
-import com.ihs.inputmethod.uimodules.ui.gif.common.ui.view.CustomSearchEditText;
 import com.ihs.inputmethod.uimodules.ui.gif.riffsy.control.DataManager;
 import com.ihs.inputmethod.uimodules.ui.gif.riffsy.dao.base.LanguageDao;
 import com.ihs.inputmethod.uimodules.ui.gif.riffsy.emojisearch.ESManager;
@@ -25,24 +17,8 @@ import com.keyboard.rainbow.thread.AsyncThreadPools;
  * Created by xu.zhang on 11/3/15.
  */
 public class LatinIME extends HSUIInputMethodService {
-    public static final String HS_NOTIFICATION_DISCONNECT_INSIDE_CONNECTION="hs.keyboard.DISCONNECT_INSIDE_CONNECTION";
-    public static final String HS_NOTIFICATION_SERVICE_DESTROY="hs.keyboard.SERVICE_DESTROY";
-    public static final String HS_NOTIFICATION_SERVICE_START_INPUT_VIEW="hs.keyboard.SERVICE_START_INPUT_VIEW";
-    public static final String HS_NOTIFICATION_PARAM_EDITOR_OWNER_PACKAGE_NAME = "editor_owner_package_name";
-    private static InputConnection insideConnection=null;
     private volatile boolean isOnDestroy=false;
 
-    private  INotificationObserver keyboardNotificationObserver = new INotificationObserver() {
-        @Override
-        public void onReceive(String eventName, HSBundle notificaiton) {
-            if (eventName.equals(Constants.HS_NOTIFICATION_START_INPUT_INSIDE)) {
-                CustomSearchEditText customSearchEditText = (CustomSearchEditText)notificaiton.getObject(Constants.HS_NOTIFICATION_CUSTOM_SEARCH_EDIT_TEXT);
-                onStartInputInside(customSearchEditText);
-            } else if (eventName.equals(Constants.HS_NOTIFICATION_FINISH_INPUT_INSIDE)) {
-                onFinishInputInside();
-            }
-        }
-    };
     @Override
     public void onCreate() {
         super.onCreate();
