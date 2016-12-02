ede43fb04207a73a188882372953d75f6d76169c
wenbinduan <wenbin.duan@ihandysoft.com>
2016-12-02 16:18:04 +0800

    merge gif

--- app/src/main/java/com/keyboard/colorkeyboard/app/SplashActivity.java
+++ app/src/main/java/com/keyboard/colorkeyboard/app/SplashActivity.java
@@ -2,7 +2,6 @@ package com.keyboard.colorkeyboard.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import com.ihs.inputmethod.api.HSDeepLinkActivity;
 import com.keyboard.colorkeyboard.R;
@@ -16,15 +15,10 @@ public final class SplashActivity extends HSDeepLinkActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                intent.setData(getIntent().getData());
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.stand, R.anim.splash);
-            }
-        }, 500);
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        intent.setData(getIntent().getData());
+        startActivity(intent);
+        finish();
+        overridePendingTransition(R.anim.stand, R.anim.splash);
     }
 }
