33d910a41df9851da501a94815378a43e8512d55
weiqianwang <weiqianwang@ihandysoft.com>
2017-05-08 21:42:08 +0800

    先启动home activity

--- app/src/main/java/com/ihs/inputmethod/api/NotificationBroadcastReceiver.java
+++ app/src/main/java/com/ihs/inputmethod/api/NotificationBroadcastReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 
 import com.ihs.commons.utils.HSLog;
 import com.ihs.inputmethod.uimodules.ui.theme.iap.IAPManager;
+import com.ihs.inputmethod.uimodules.ui.theme.ui.ThemeHomeActivity;
 
 /**
  * Created by Arthur on 17/5/6.
@@ -16,6 +17,10 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Intent intent1 = new Intent(context, ThemeHomeActivity.class);
+        intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent1);
+        
         HSLog.e(intent.getStringExtra("eventName") + intent.toString());
         switch (intent.getStringExtra("eventName")) {
             case "Charging":
