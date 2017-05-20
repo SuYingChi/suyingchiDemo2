4069169607dc22adb3528ff19dcbaeaf9f8a3b7e
weiqianwang <weiqianwang@ihandysoft.com>
2017-05-20 14:51:58 +0800

    暂时取消boost

--- app/src/main/java/com/ihs/inputmethod/feature/boost/BoostSource.java
+++
@@ -1,7 +0,0 @@
-package com.ihs.inputmethod.feature.boost;
-
-public enum BoostSource {
-    IconDrawable,
-    BoostActivity,
-    LockerToggle
-}
--- app/src/main/java/com/ihs/inputmethod/feature/boost/BoostType.java
+++
@@ -1,7 +0,0 @@
-package com.ihs.inputmethod.feature.boost;
-
-public enum BoostType {
-    RAM,
-    BATTERY,
-    CPU_TEMPERATURE,
-}
