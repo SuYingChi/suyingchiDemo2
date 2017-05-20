19bf41b37c70b20643101ebc261d61baacaeafec
weiqianwang <weiqianwang@ihandysoft.com>
2017-05-20 19:55:52 +0800

    fix font not correct

--- app/src/main/java/com/ihs/inputmethod/feature/lucky/LuckyActivity.java
+++ app/src/main/java/com/ihs/inputmethod/feature/lucky/LuckyActivity.java
@@ -23,6 +23,7 @@ import com.ihs.app.framework.HSApplication;
 import com.ihs.app.framework.activity.HSAppCompatActivity;
 import com.ihs.commons.utils.HSLog;
 import com.ihs.commons.utils.HSPreferenceHelper;
+import com.ihs.inputmethod.api.analytics.HSGoogleAnalyticsUtils;
 import com.ihs.inputmethod.feature.common.ActivityUtils;
 import com.ihs.inputmethod.feature.common.AnimatorListenerAdapter;
 import com.ihs.inputmethod.feature.common.Utils;
@@ -154,6 +155,8 @@ public class LuckyActivity extends HSAppCompatActivity
         mMusicPlayer = new MusicPlayer(mute);
         mMusicHandler = new MusicPlayerHandler();
         initView(mute);
+
+        HSGoogleAnalyticsUtils.getInstance().logAppEvent("lucky_open");
     }
 
 
