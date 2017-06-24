1bbc7318a42a535c768b5b540c411d4e6c04a9c6
jixiang.chen <jixiang.chen@ihandysoft.com>
2017-06-24 19:20:27 +0800

    去掉一层无用布局

--- app/src/main/java/com/ihs/inputmethod/uimodules/ui/theme/ui/customtheme/modules/sound/SoundProvider.java
+++ app/src/main/java/com/ihs/inputmethod/uimodules/ui/theme/ui/customtheme/modules/sound/SoundProvider.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.ihs.inputmethod.uimodules.R;
 import com.ihs.inputmethod.uimodules.ui.theme.ui.customtheme.base.BaseThemeItemProvider;
 import com.keyboard.core.themes.custom.KCElementResourseHelper;
 import com.keyboard.core.themes.custom.elements.KCBaseElement;
@@ -41,7 +42,7 @@ public final class SoundProvider extends BaseThemeItemProvider<KCSoundElement, B
 	protected void onBindViewHolder(@NonNull BaseItemHolder holder, @NonNull Object item) {
 		super.onBindViewHolder(holder, item);
 		DisplayMetrics displayMetrics = holder.itemView.getResources().getDisplayMetrics();
-		int height = Math.min(displayMetrics.widthPixels,displayMetrics.heightPixels) / fragment.SPAN_COUNT;
+		int height = Math.min(displayMetrics.widthPixels,displayMetrics.heightPixels) / fragment.SPAN_COUNT  -  holder.itemView.getResources().getDimensionPixelSize(R.dimen.custom_theme_item_margin) *2;
 		int width = height;
 		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
 		layoutParams.gravity = Gravity.CENTER;
