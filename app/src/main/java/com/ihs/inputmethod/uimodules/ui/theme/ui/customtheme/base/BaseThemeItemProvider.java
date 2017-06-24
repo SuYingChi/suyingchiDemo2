87a5fdce4c9b88b973d9aa809166c8499a9ede83
weiqianwang <weiqianwang@ihandysoft.com>
2017-06-24 16:53:29 +0800

    change applying text to downloading

--- app/src/main/java/com/ihs/inputmethod/uimodules/ui/theme/ui/customtheme/base/BaseThemeItemProvider.java
+++ app/src/main/java/com/ihs/inputmethod/uimodules/ui/theme/ui/customtheme/base/BaseThemeItemProvider.java
@@ -150,7 +150,7 @@ public abstract class BaseThemeItemProvider<I extends Object, V extends BaseThem
             if (backgroundDrawable == null) {
                 backgroundDrawable = new ColorDrawable(Color.BLACK);
             }
-            adLoadingView.configParams(backgroundDrawable, baseElement.getPreview(), "Applying...", "Applied Successfully", adPlacementName, new AdLoadingView.OnAdBufferingListener() {
+            adLoadingView.configParams(backgroundDrawable, baseElement.getPreview(), "Downloading...", "Applied Successfully", adPlacementName, new AdLoadingView.OnAdBufferingListener() {
 
                 @Override
                 public void onDismiss(boolean success) {
