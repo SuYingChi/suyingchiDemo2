0a25fde32a2bfa9e8318081c29143e77fad5395e
yanxia <yan.xia@ihandysoft.com>
2017-06-26 13:29:51 +0800

    add construct

--- app/src/main/java/com/ihs/inputmethod/adpanel/KeyboardGooglePlayAdManager.java
+++ app/src/main/java/com/ihs/inputmethod/adpanel/KeyboardGooglePlayAdManager.java
@@ -7,6 +7,7 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import com.ihs.app.framework.HSApplication;
+import com.ihs.inputmethod.api.utils.HSDisplayUtils;
 import com.ihs.inputmethod.uimodules.R;
 import com.ihs.keyboardutils.nativeads.NativeAdParams;
 import com.ihs.keyboardutils.nativeads.NativeAdView;
@@ -15,21 +16,32 @@ import com.ihs.keyboardutils.nativeads.NativeAdView;
  * Created by xiayan on 2017/6/25.
  */
 
-public class KeyboardGooglePlayAdManager {
+public class KeyboardGooglePlayAdManager implements NativeAdView.OnAdLoadedListener{
+    private String adPlacement;
     private NativeAdView nativeAdView;
     private ProgressBar progressBar;
+    private int width;
+
+    public KeyboardGooglePlayAdManager(String adPlacement) {
+        this.adPlacement = adPlacement;
+    }
 
     public void init() {
+        width = HSDisplayUtils.getScreenWidthForContent() - HSDisplayUtils.dip2px(16);
         View view = LayoutInflater.from(HSApplication.getContext()).inflate(R.layout.ad_style_1, null);
         LinearLayout loadingView = (LinearLayout) LayoutInflater.from(HSApplication.getContext()).inflate(R.layout.ad_loading_3, null);
         LinearLayout.LayoutParams loadingLP = new LinearLayout.LayoutParams(width, (int) (width / 1.9f));
         loadingView.setLayoutParams(loadingLP);
         loadingView.setGravity(Gravity.CENTER);
         nativeAdView = new NativeAdView(HSApplication.getContext(), view, loadingView);
-
     }
 
     public void loadAd() {
-        nativeAdView.configParams(new NativeAdParams(HSApplication.getContext().getString(R.string.ad_placement_themetryad), width, 1.9f));
+        nativeAdView.configParams(new NativeAdParams(adPlacement, width, 1.9f));
+    }
+
+    @Override
+    public void onAdLoaded(NativeAdView nativeAdView) {
+
     }
 }
