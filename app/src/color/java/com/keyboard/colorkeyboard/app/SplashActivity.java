f70dd5591ead4e1166dfa436b377fe697b71d098
jixiang.chen <jixiang.chen@ihandysoft.com>
2017-07-13 17:26:25 +0800

    去除libTopAppMonitor库

--- app/src/color/java/com/keyboard/colorkeyboard/app/SplashActivity.java
+++ app/src/color/java/com/keyboard/colorkeyboard/app/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.keyboard.common;
+package com.keyboard.colorkeyboard.app;
 
 // 保持类名位置以保证原来的图标可用
 public final class SplashActivity extends com.keyboard.common.SplashActivity {
--- app/src/main/java/com/ihs/inputmethod/accessbility/AccessibilityEventListener.java
+++ app/src/main/java/com/ihs/inputmethod/accessbility/AccessibilityEventListener.java
@@ -9,9 +9,9 @@ import android.provider.Settings;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.inputmethod.InputMethodManager;
 
-import com.acb.appmonitor.libaccessibilityservice.IAccEventListener;
 import com.ihs.app.framework.HSApplication;
 import com.ihs.commons.utils.HSLog;
+import com.ihs.device.accessibility.service.IAccEventListener;
 import com.ihs.inputmethod.api.framework.HSInputMethodListManager;
 
 import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
@@ -23,7 +23,6 @@ import static android.content.Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED;
 import static com.ihs.inputmethod.accessbility.AccGALogger.app_permission_accessibility_allowed;
 import static com.ihs.inputmethod.accessbility.AccGALogger.logOneTimeGA;
 
-//import com.ihs.devicemonitor.accessibility.IAccEventListener;
 
 /**
  * Created by Arthur on 16/12/28.
@@ -34,12 +33,11 @@ public class AccessibilityEventListener implements IAccEventListener {
     public static final int MODE_SETUP_KEYBOARD = 1;
 
     private int modeCode = -1;
+    private SetupKeyboardAccessibilityService accessibilityService;
 
     public AccessibilityEventListener(int code) {
         modeCode = code;
     }
-
-    private SetupKeyboardAccessibilityService accessibilityService;
 
     @Override
     public IBinder asBinder() {
