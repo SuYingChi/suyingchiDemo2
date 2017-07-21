1a2f1b8b27edba41a50e029e12612b41308c76cb
weiqianwang <weiqianwang@ihandysoft.com>
2017-07-21 15:26:28 +0800

    init

--- app/src/main/java/com/ihs/inputmethod/api/HSUIApplication.java
+++ app/src/main/java/com/ihs/inputmethod/api/HSUIApplication.java
@@ -11,6 +11,7 @@ import android.os.Build;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.acb.call.AcbCallManager;
 import com.acb.expressads.AcbExpressAdManager;
 import com.acb.interstitialads.AcbInterstitialAdManager;
 import com.acb.nativeads.AcbNativeAdManager;
@@ -242,6 +243,8 @@ public class HSUIApplication extends HSInputMethodApplication {
         }
         ActivityLifecycleMonitor.startMonitor(this);
         activeAdPlacements();
+
+        AcbCallManager.initWithDefaultFactory();
     }
 
     private void activeAdPlacements() {
