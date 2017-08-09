58ff6e6fa9d277885a2425683a51c47c25889122
rongbo.qi <rongbo.qi@ihandysoft.com>
2017-08-09 15:00:57 +0800

    调整ZipTool内容，使脚本可以直接运行

--- app/ziptool/build_apk_color.sh
+++ app/ziptool/build_apk_color.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sed -i "" 's/com.*.R/com.keyboard.colorkeyboard.R/g' config_color.xml
-java -jar ./AndResGuard*.jar ../build/outputs/apk/app-color-release.apk -config config_color.xml -out ../build/outputs/apk/app-color-zip
+java -jar ./AndResGuard*.jar ../build/outputs/apk/app-color-release.apk -config config_color.xml -out ../build/outputs/apk/app-color-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
--- app/ziptool/build_apk_smiley.sh
+++ app/ziptool/build_apk_smiley.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sed -i "" 's/com.*.R/com.smartkeyboard.emoji.R/g' config_smiley.xml
-java -jar ./AndResGuard*.jar ../build/outputs/apk/app-smiley-release.apk -config config_smiley.xml -out ../build/outputs/apk/app-smiley-zip
+java -jar ./AndResGuard*.jar ../build/outputs/apk/app-smiley-release.apk -config config_smiley.xml -out ../build/outputs/apk/app-smiley-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
