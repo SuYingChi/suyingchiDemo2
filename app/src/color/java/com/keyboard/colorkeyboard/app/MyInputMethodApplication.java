47c4a76b406a455bc9576745c1c6e7d23099cd41
yanxia <yan.xia@ihandysoft.com>
2017-08-15 14:31:49 +0800

    remove launcher activity launchMode & correct splash activity

--- app/src/color/java/com/keyboard/colorkeyboard/app/MyInputMethodApplication.java
+++ app/src/color/java/com/keyboard/colorkeyboard/app/MyInputMethodApplication.java
@@ -3,7 +3,6 @@ package com.keyboard.colorkeyboard.app;
 import android.app.Activity;
 
 import com.ihs.inputmethod.api.HSUIApplication;
-import com.keyboard.common.SplashActivity;
 
 
 public class MyInputMethodApplication extends HSUIApplication {
