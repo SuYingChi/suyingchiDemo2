98c242692ff20b99bcf9173d488ea40d112c4f80
guonan.lv <guonan.lv@ihandysoft.com>
2017-08-21 14:05:11 +0800

    add getter and setter

--- app/src/main/java/com/ihs/inputmethod/uimodules/ui/fonts/homeui/FontModel.java
+++ app/src/main/java/com/ihs/inputmethod/uimodules/ui/fonts/homeui/FontModel.java
@@ -13,11 +13,17 @@ public class FontModel {
     private static final String FONT_FILE_SUFFIX = ".json";
 
     private boolean needDownload = true;
+    private String fontName;
 
     public FontModel(HSSpecialCharacter specialCharacter) {
         hsSpecialCharacter = specialCharacter;
         needDownload = true;
+    }
+
+    public FontModel(String fontName) {
+        this.fontName = fontName;
         baseDownloadUrl = HSConfig.getString("Application", "Server", "FontDownloadBaseURL");
+        needDownload = true;
     }
 
     public HSSpecialCharacter getHsSpecialCharacter() {
@@ -33,7 +39,7 @@ public class FontModel {
     }
 
     public String getFontDownloadBaseURL() {
-        return null;
+        return fontName == null ? baseDownloadUrl+fontName+FONT_FILE_SUFFIX : null;
     }
 
 }
