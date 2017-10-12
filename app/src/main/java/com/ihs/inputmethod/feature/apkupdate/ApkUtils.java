cca314dde22808007dd620c6767623050f8733c3
yanxia <yan.xia@ihandysoft.com>
2017-10-12 16:24:30 +0800

    adjust alert UI

--- app/src/main/java/com/ihs/inputmethod/feature/apkupdate/ApkUtils.java
+++ app/src/main/java/com/ihs/inputmethod/feature/apkupdate/ApkUtils.java
@@ -334,7 +334,7 @@ public class ApkUtils {
         message.setText(HSConfig.optString(HSApplication.getContext().getString(R.string.custom_share_alert_message), "Application", "Update", "ShareAlert", "Message", preferredLanguageString));
         Button positiveBtn = (Button) view.findViewById(R.id.btn_share);
         positiveBtn.setBackgroundDrawable(RippleDrawableUtils.getContainDisableStatusCompatRippleDrawable(
-                HSApplication.getContext().getResources().getColor(R.color.custom_rate_alert_button_bg),
+                HSApplication.getContext().getResources().getColor(R.color.custom_share_alert_button_bg),
                 HSApplication.getContext().getResources().getColor(R.color.guide_bg_disable_color),
                 HSApplication.getContext().getResources().getDimension(R.dimen.apk_update_alert_button_radius)));
         positiveBtn.setText(HSConfig.optString(HSApplication.getContext().getString(R.string.custom_share_alert_button_text), "Application", "Update", "ShareAlert", "ButtonText", preferredLanguageString));
