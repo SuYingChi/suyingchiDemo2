928a64a3918ca49546d9a77d838353b82ddd2883
jixiang.chen <jixiang.chen@ihandysoft.com>
2017-10-20 11:23:27 +0800

    键盘facemoji显示适配

--- app/src/main/java/com/ihs/inputmethod/uimodules/ui/facemoji/FacemojiPageGridAdapter.java
+++ app/src/main/java/com/ihs/inputmethod/uimodules/ui/facemoji/FacemojiPageGridAdapter.java
@@ -17,110 +17,113 @@ import java.util.List;
  * Created by dsapphire on 15/11/27.
  */
 public class FacemojiPageGridAdapter extends BaseAdapter implements FacemojiView.OnFacemojiEventListener,
-		Recoverable {
+        Recoverable {
 
-	private FacemojiPageGridView.OnFacemojiClickListener mListener;
-	private List<FacemojiSticker> mData;
-	private int stickerDimension;
-	private LayoutInflater mInflater;
+    private FacemojiPageGridView.OnFacemojiClickListener mListener;
+    private List<FacemojiSticker> mData;
+    private int stickerWidth;
+    private int stickerHeight;
+    private LayoutInflater mInflater;
 
-	public FacemojiPageGridAdapter(final List<FacemojiSticker> data,
-								   FacemojiPageGridView.OnFacemojiClickListener listener,
-								   final Context context,
-								   final int dimension) {
-		this.mData = data;
-		this.mListener = listener;
-		this.mInflater = LayoutInflater.from(context);
-		this.stickerDimension = dimension;
-	}
+    public FacemojiPageGridAdapter(final List<FacemojiSticker> data,
+                                   FacemojiPageGridView.OnFacemojiClickListener listener,
+                                   final Context context,
+                                   final int stickerWidth, int stickerHeight) {
+        this.mData = data;
+        this.mListener = listener;
+        this.mInflater = LayoutInflater.from(context);
+        this.stickerWidth = stickerWidth;
+        this.stickerHeight = stickerHeight;
+    }
 
-	@Override
-	public int getCount() {
-		if (mData == null) return 0;
-		return mData.size();
-	}
+    @Override
+    public int getCount() {
+        if (mData == null) return 0;
+        return mData.size();
+    }
 
-	@Override
-	public Object getItem(int position) {
-		if(mData!=null){
-			if(mData.size()>position)
-				return mData.get(position);
-		}
-		return null;
-	}
+    @Override
+    public Object getItem(int position) {
+        if (mData != null) {
+            if (mData.size() > position)
+                return mData.get(position);
+        }
+        return null;
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		StickerViewHolder holder;
-		FacemojiSticker sticker = (FacemojiSticker) getItem(position);
-		if (convertView != null) {
-			holder = (StickerViewHolder) convertView.getTag();
-			if (!sticker.equals(holder.facemojiView.getSticker())) {
-				holder.facemojiView.setSticker(sticker);
-				holder.facemojiView.setTag(sticker);
-				((FacemojiView) convertView).setOnFacemojiEventListener(this);
-				convertView.setTag(holder);
-			}
-			return convertView;
-		}
-		convertView = mInflater.inflate(R.layout.facemoji_custom_view, null);
-		final View containerLayout = convertView.findViewById(R.id.facemoji_layout);
-		containerLayout.setLayoutParams(new GridView.LayoutParams((int)(stickerDimension), (int)(stickerDimension)));
-		final FacemojiAnimationView facemojiView = (FacemojiAnimationView) containerLayout.findViewById(R.id.sticker_player_view);
-		facemojiView.setSticker(sticker);
-		facemojiView.setTag(sticker);
-		((FacemojiView) convertView).setOnFacemojiEventListener(this);
-		holder = new StickerViewHolder();
-		holder.facemojiView = facemojiView;
-		convertView.setTag(holder);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        StickerViewHolder holder;
+        FacemojiSticker sticker = (FacemojiSticker) getItem(position);
+        if (convertView != null) {
+            holder = (StickerViewHolder) convertView.getTag();
+            if (!sticker.equals(holder.facemojiView.getSticker())) {
+                holder.facemojiView.setSticker(sticker);
+                holder.facemojiView.setTag(sticker);
+                ((FacemojiView) convertView).setOnFacemojiEventListener(this);
+                convertView.setTag(holder);
+            }
+            return convertView;
+        }
+        convertView = mInflater.inflate(R.layout.facemoji_custom_view, null);
+        final View containerLayout = convertView.findViewById(R.id.facemoji_layout);
+        containerLayout.setLayoutParams(new GridView.LayoutParams(stickerWidth, stickerHeight));
+        final FacemojiAnimationView facemojiView = (FacemojiAnimationView) containerLayout.findViewById(R.id.sticker_player_view);
+        facemojiView.setSticker(sticker);
+        facemojiView.setTag(sticker);
+        ((FacemojiView) convertView).setOnFacemojiEventListener(this);
+        holder = new StickerViewHolder();
+        holder.facemojiView = facemojiView;
+        convertView.setTag(holder);
 
-		return convertView;
-	}
+        return convertView;
+    }
 
-	@Override
-	public void onPressFacemoji() {}
+    @Override
+    public void onPressFacemoji() {
+    }
 
-	@Override
-	public void onReleaseFacemoji(final FacemojiSticker facemojiSticker) {
-		if (facemojiSticker != null) {
-			mListener.onFacemojiClicked(facemojiSticker);
-		}
-	}
+    @Override
+    public void onReleaseFacemoji(final FacemojiSticker facemojiSticker) {
+        if (facemojiSticker != null) {
+            mListener.onFacemojiClicked(facemojiSticker);
+        }
+    }
 
-	public void setData(List<FacemojiSticker> data){
-		mData=data;
-	}
+    public void setData(List<FacemojiSticker> data) {
+        mData = data;
+    }
 
-	class StickerViewHolder {
-		public FacemojiAnimationView facemojiView;
-	}
+    class StickerViewHolder {
+        public FacemojiAnimationView facemojiView;
+    }
 
-	@Override
-	public void save() {
-		mDataSnapshot.clear();
-		mDataSnapshot.addAll(mData);
-	}
+    @Override
+    public void save() {
+        mDataSnapshot.clear();
+        mDataSnapshot.addAll(mData);
+    }
 
-	@Override
-	public void release() {
-		mData.clear();
-	}
+    @Override
+    public void release() {
+        mData.clear();
+    }
 
-	@Override
-	public void restore() {
-		mData.addAll(mDataSnapshot);
-		mDataSnapshot.clear();
-	}
+    @Override
+    public void restore() {
+        mData.addAll(mDataSnapshot);
+        mDataSnapshot.clear();
+    }
 
-	@Override
-	public Recoverable.State currentState() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public Recoverable.State currentState() {
+        throw new UnsupportedOperationException();
+    }
 
-	private List<FacemojiSticker> mDataSnapshot = new ArrayList<>() ;
+    private List<FacemojiSticker> mDataSnapshot = new ArrayList<>();
 }
--- app/src/main/java/com/ihs/inputmethod/uimodules/ui/facemoji/FacemojiPalettesAdapter.java
+++ app/src/main/java/com/ihs/inputmethod/uimodules/ui/facemoji/FacemojiPalettesAdapter.java
@@ -1,179 +1,204 @@
 package com.ihs.inputmethod.uimodules.ui.facemoji;
 
+import android.content.res.Resources;
 import android.support.v4.view.PagerAdapter;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ihs.app.framework.HSApplication;
 import com.ihs.commons.utils.HSLog;
 import com.ihs.inputmethod.uimodules.R;
+import com.ihs.inputmethod.uimodules.ui.facemoji.bean.FacemojiSticker;
+
+import java.util.List;
 
 class FacemojiPalettesAdapter extends PagerAdapter implements Recoverable {
-	private FacemojiManager.FacemojiType facemojiType = FacemojiManager.FacemojiType.CLASSIC;
-	private static String TAG = "FacemojiPalettesAdapter";
-	private SparseArray<FacemojiPageGridView> mActivePageViews = new SparseArray<>();
-	private FacemojiLayoutParams mImageLayoutParams;
+    private FacemojiManager.FacemojiType facemojiType = FacemojiManager.FacemojiType.CLASSIC;
+    private static String TAG = "FacemojiPalettesAdapter";
+    private SparseArray<FacemojiPageGridView> mActivePageViews = new SparseArray<>();
+    private FacemojiLayoutParams mImageLayoutParams;
 
-	private int mActivePosition = 0;
-	private FacemojiPalettesView mFacemojiPalettesView;
+    private int mActivePosition = 0;
+    private FacemojiPalettesView mFacemojiPalettesView;
 
-	public FacemojiPalettesAdapter(FacemojiPalettesView facemojiPalettesView,
-								   FacemojiLayoutParams imageLayoutParams) {
-		mFacemojiPalettesView = facemojiPalettesView;
-		mImageLayoutParams = imageLayoutParams;
-	}
+    public FacemojiPalettesAdapter(FacemojiPalettesView facemojiPalettesView,
+                                   FacemojiLayoutParams imageLayoutParams) {
+        mFacemojiPalettesView = facemojiPalettesView;
+        mImageLayoutParams = imageLayoutParams;
+    }
 
-	public void setFacemojiType(FacemojiManager.FacemojiType facemojiType) {
-		this.facemojiType = facemojiType;
-	}
+    public void setFacemojiType(FacemojiManager.FacemojiType facemojiType) {
+        this.facemojiType = facemojiType;
+    }
 
-	@Override
-	public int getCount() {
-		return FacemojiManager.getInstance().getTotalPageCount(facemojiType);
-	}
+    @Override
+    public int getCount() {
+        return FacemojiManager.getInstance().getTotalPageCount(facemojiType);
+    }
 
-	@Override
-	public int getItemPosition(Object object) {
-		return POSITION_NONE;
-	}
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
 
-	@Override
-	public void setPrimaryItem(final ViewGroup container, final int position, final Object object) {
-		if (mActivePosition == position) {
-			return;
-		}
-		mActivePosition = position;
-	}
+    @Override
+    public void setPrimaryItem(final ViewGroup container, final int position, final Object object) {
+        if (mActivePosition == position) {
+            return;
+        }
+        mActivePosition = position;
+    }
 
-	public void clear() {
-		mActivePageViews.clear();
-	}
+    public void clear() {
+        mActivePageViews.clear();
+    }
 
-	@Override
-	public Object instantiateItem(final ViewGroup container, final int position) {
-		FacemojiPageGridView view = mActivePageViews.get(position);
-		if (view != null) {
-			container.addView(view);
-			return view;
-		}
-		mActivePageViews.remove(position);
-		final LayoutInflater inflater = LayoutInflater.from(container.getContext());
-		final FacemojiPageGridView gifPageGridView = (FacemojiPageGridView)inflater.inflate(R.layout.facemoji_page_view, container, false);
-		mImageLayoutParams.setPageGridViewProperties(gifPageGridView);
-		FacemojiPageGridAdapter adapter=new FacemojiPageGridAdapter(
-											FacemojiManager.getInstance().getDataFromPagePosition(facemojiType,position),
-											mFacemojiPalettesView,
-											container.getContext(),
-											Math.min(mImageLayoutParams.getViewWidth(), mImageLayoutParams.getViewHeight()));
-		gifPageGridView.setAdapter(adapter);
-		container.addView(gifPageGridView);
-		mActivePageViews.put(position, gifPageGridView);
-		return gifPageGridView;
-	}
+    @Override
+    public Object instantiateItem(final ViewGroup container, final int position) {
+        FacemojiPageGridView view = mActivePageViews.get(position);
+        if (view != null) {
+            container.addView(view);
+            return view;
+        }
+        mActivePageViews.remove(position);
+        final LayoutInflater inflater = LayoutInflater.from(container.getContext());
+        final FacemojiPageGridView gifPageGridView = (FacemojiPageGridView) inflater.inflate(R.layout.facemoji_page_view, container, false);
 
-	@Override
-	public boolean isViewFromObject(final View view, final Object object) {
-		return view == object;
-	}
+        List<FacemojiSticker> data = FacemojiManager.getInstance().getDataFromPagePosition(facemojiType, position);
 
-	@Override
-	public void destroyItem(final ViewGroup container, final int position, final Object object) {
-		container.removeView(mActivePageViews.get(position));
-		mActivePageViews.remove(position);
-	}
+        int height;
+        int width;
+        if (data.get(0).getWidth() == data.get(0).getHeight()){
+            mImageLayoutParams.setPageGridViewProperties(gifPageGridView);
+            height = width = Math.min(mImageLayoutParams.getViewWidth(),mImageLayoutParams.getViewHeight());
+        }else {
+            Resources resources = HSApplication.getContext().getResources();
+            int verticalSpacing = resources.getDimensionPixelSize(R.dimen.facemoji_keyboard_grid_vertical_space);
+            int horizontalSpacing = resources.getDimensionPixelSize(R.dimen.facemoji_keyboard_grid_horizontal_space);
+            int columns = 3;
+            gifPageGridView.setVerticalSpacing(verticalSpacing);
+            gifPageGridView.setHorizontalSpacing(horizontalSpacing);
+            gifPageGridView.setNumColumns(columns);
+
+            height = (container.getMeasuredHeight() - verticalSpacing) / 2;
+            width = (container.getMeasuredWidth() - horizontalSpacing * (columns - 1)) / columns;
+        }
+
+        FacemojiPageGridAdapter adapter = new FacemojiPageGridAdapter(
+                data,
+                mFacemojiPalettesView,
+                container.getContext(),
+                width, height);
+        gifPageGridView.setAdapter(adapter);
+        container.addView(gifPageGridView);
+        mActivePageViews.put(position, gifPageGridView);
+        return gifPageGridView;
+    }
+
+    @Override
+    public boolean isViewFromObject(final View view, final Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(final ViewGroup container, final int position, final Object object) {
+        container.removeView(mActivePageViews.get(position));
+        mActivePageViews.remove(position);
+    }
 
 
-	private boolean isCurrentPage(final int position) {
-		return position == mFacemojiPalettesView.getCurrentPagerPosition();
-	}
+    private boolean isCurrentPage(final int position) {
+        return position == mFacemojiPalettesView.getCurrentPagerPosition();
+    }
 
-	public void pauseAnimation() {
-		stopAnimation(mActivePosition);
-	}
+    public void pauseAnimation() {
+        stopAnimation(mActivePosition);
+    }
 
-	public void resumeAnimation() {
-		startAnimation(mActivePosition);
-	}
+    public void resumeAnimation() {
+        startAnimation(mActivePosition);
+    }
 
-	public void startAnimation(final int pageId) {
-		HSLog.d("start animation for page " + pageId);
+    public void startAnimation(final int pageId) {
+        HSLog.d("start animation for page " + pageId);
 
-		FacemojiPageGridView facemojiPageGridView = mActivePageViews.get(pageId);
-		if (facemojiPageGridView == null) {
-			return;
-		}
+        FacemojiPageGridView facemojiPageGridView = mActivePageViews.get(pageId);
+        if (facemojiPageGridView == null) {
+            return;
+        }
 
-		if (facemojiPageGridView.getChildCount() == 0) {
-			facemojiPageGridView.postDelayed(new Runnable() {
-				@Override
-				public void run() {
-					HSLog.d("start animation for page delayed... " + pageId);
-					startAnimation(pageId);
-				}
-			}, 50);
-		}
+        if (facemojiPageGridView.getChildCount() == 0) {
+            facemojiPageGridView.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    HSLog.d("start animation for page delayed... " + pageId);
+                    startAnimation(pageId);
+                }
+            }, 50);
+        }
 
-		HSLog.d("child count" + facemojiPageGridView.getChildCount());
+        HSLog.d("child count" + facemojiPageGridView.getChildCount());
 
-		for (int i = 0; i < facemojiPageGridView.getChildCount(); ++i) {
-			FacemojiView facemojiView = (FacemojiView) facemojiPageGridView.getChildAt(i);
-			facemojiView.startAnimation();
-		}
-	}
+        for (int i = 0; i < facemojiPageGridView.getChildCount(); ++i) {
+            FacemojiView facemojiView = (FacemojiView) facemojiPageGridView.getChildAt(i);
+            facemojiView.startAnimation();
+        }
+    }
 
-	public void stopAllAnimations() {
-		for (int i = 0; i < mActivePageViews.size(); ++i) {
-			stopAnimation(i);
-		}
-	}
+    public void stopAllAnimations() {
+        for (int i = 0; i < mActivePageViews.size(); ++i) {
+            stopAnimation(i);
+        }
+    }
 
-	/**
-	 * Stop animation of the page
-	 * @param pageId
+    /**
+     * Stop animation of the page
+     *
+     * @param pageId
      */
-	private void stopAnimation(int pageId) {
-		FacemojiPageGridView facemojiPageGridView = mActivePageViews.get(pageId);
+    private void stopAnimation(int pageId) {
+        FacemojiPageGridView facemojiPageGridView = mActivePageViews.get(pageId);
 
-		if (facemojiPageGridView == null) {
-			HSLog.d("failed to stop animation for page " + pageId);
-			return;
-		}
+        if (facemojiPageGridView == null) {
+            HSLog.d("failed to stop animation for page " + pageId);
+            return;
+        }
 
-		HSLog.d("stop animation for page " + pageId);
-		HSLog.d("child count" + facemojiPageGridView.getChildCount());
+        HSLog.d("stop animation for page " + pageId);
+        HSLog.d("child count" + facemojiPageGridView.getChildCount());
 
-		for (int i = 0; i < facemojiPageGridView.getChildCount(); ++i) {
-			FacemojiView facemojiView = (FacemojiView) facemojiPageGridView.getChildAt(i);
-			facemojiView.stopAnimation();
-		}
-	}
+        for (int i = 0; i < facemojiPageGridView.getChildCount(); ++i) {
+            FacemojiView facemojiView = (FacemojiView) facemojiPageGridView.getChildAt(i);
+            facemojiView.stopAnimation();
+        }
+    }
 
-	@Override
-	public void save() {
-		for (int i = 0, len = mActivePageViews.size(); i < len; i++) {
-			mActivePageViews.valueAt(i).save();
-		}
-	}
+    @Override
+    public void save() {
+        for (int i = 0, len = mActivePageViews.size(); i < len; i++) {
+            mActivePageViews.valueAt(i).save();
+        }
+    }
 
-	@Override
-	public void restore() {
-		for (int i = 0, len = mActivePageViews.size(); i < len; i++) {
-			mActivePageViews.valueAt(i).restore();
-		}
-	}
+    @Override
+    public void restore() {
+        for (int i = 0, len = mActivePageViews.size(); i < len; i++) {
+            mActivePageViews.valueAt(i).restore();
+        }
+    }
 
-	@Override
-	public void release()
-	{
-		for (int i = 0, len = mActivePageViews.size(); i < len; i++) {
+    @Override
+    public void release() {
+        for (int i = 0, len = mActivePageViews.size(); i < len; i++) {
             mActivePageViews.valueAt(i).release();
-		}
-	}
+        }
+    }
 
-	@Override
-	public Recoverable.State currentState() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public Recoverable.State currentState() {
+        throw new UnsupportedOperationException();
+    }
 
 }
