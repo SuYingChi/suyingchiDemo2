ab72028abe129dbb34e506cca6a6e4a7d2b7ff78
rongbo.qi <rongbo.qi@ihandysoft.com>
2017-11-04 16:24:19 +0800

    更改Grin相关配置

--- app/ziptool/build_apk_grin.sh
+++ app/ziptool/build_apk_grin.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sed -i "" 's/com.*.R/com.mobipioneer.emojikeyboard.R/g' config.xml
-java -jar ./AndResGuard*.jar ../build/outputs/apk/app-grin-release.apk -config config.xml -signature ../Mobipioneer zdhszyzs zdhszyzs mobipioneer -out ../build/outputs/apk/app-grin-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
+sed -i "" 's/com.*.R/com.keyboard.font.theme.emoji.R/g' config.xml
+java -jar ./AndResGuard*.jar ../build/outputs/apk/app-grin-release.apk -config config.xml -signature ../KeyboardArts zdhszyzs zdhszyzs keyboardarts -out ../build/outputs/apk/app-grin-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
