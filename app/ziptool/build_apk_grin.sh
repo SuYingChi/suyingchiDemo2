d628651044d8f81c6bee66cdde30f0929a50668b
weiqianwang <weiqianwang@ihandysoft.com>
2017-11-11 20:14:24 +0800

    pipline编译不通过 还原配置

--- app/ziptool/build_apk_grin.sh
+++ app/ziptool/build_apk_grin.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sed -i "" 's/com.*.R/com.keyboard.font.theme.emoji.R/g' config.xml
-java -jar ./AndResGuard*.jar ../build/outputs/apk/app-grin-armeabi-v7a-release.apk -config config.xml -signature ../KeyboardArts zdhszyzs zdhszyzs keyboardarts -out ../build/outputs/apk/grin/release/app-grin-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
+java -jar ./AndResGuard*.jar ../build/outputs/apk/app-grin-release.apk -config config.xml -signature ../KeyboardArts zdhszyzs zdhszyzs keyboardarts -out ../build/outputs/apk/app-grin-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
