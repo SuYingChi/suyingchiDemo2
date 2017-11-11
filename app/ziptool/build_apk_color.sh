69210ca95bd5070d8e6f780cb0c04376afa4a82b
weiqianwang <weiqianwang@ihandysoft.com>
2017-11-11 15:01:44 +0800

    升级script

--- app/ziptool/build_apk_color.sh
+++ app/ziptool/build_apk_color.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sed -i "" 's/com.*.R/com.keyboard.colorkeyboard.R/g' config.xml
-java -jar ./AndResGuard*.jar ../build/outputs/apk/app-color-release.apk -config config.xml -signature ../KeyboardArts zdhszyzs zdhszyzs keyboardarts -out ../build/outputs/apk/app-color-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
+java -jar ./AndResGuard*.jar ../build/outputs/apk/app-color-armeabi-v7a-release.apk -config config.xml -signature ../KeyboardArts zdhszyzs zdhszyzs keyboardarts -out ../build/outputs/apk/app-color-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
--- app/ziptool/build_apk_smiley.sh
+++ app/ziptool/build_apk_smiley.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sed -i "" 's/com.*.R/com.smartkeyboard.emoji.R/g' config.xml
-java -jar ./AndResGuard*.jar ../build/outputs/apk/app-smiley-release.apk -config config.xml -signature ../EmojiLabs zdhszyzs zdhszyzs emoji -out ../build/outputs/apk/app-smiley-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
+java -jar ./AndResGuard*.jar ../build/outputs/apk/app-smiley-armeabi-v7a-release.apk -config config.xml -signature ../EmojiLabs zdhszyzs zdhszyzs emoji -out ../build/outputs/apk/app-smiley-zip -7zip ./SevenZip-osx-x86_64.exe -zipalign ./zipalign
