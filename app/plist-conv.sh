b472a15717dd2c9a6199d889f233cca6bb2cba0b
jixiang.chen <jixiang.chen@ihandysoft.com>
2017-11-14 19:37:02 +0800

    修改alert使用pa，同时修改pileLine apk导出目录

--- app/plist-conv.sh
+++ app/plist-conv.sh
@@ -1,6 +1,13 @@
 chmod +x ./pacoder
 chmod +x ./yacoder
 
+for file in `find ./build/intermediates/assets -type f -name "Alerts.??.plist"`
+do
+    echo $file
+    ./pacoder -e $file ${file%.*}
+    rm $file
+done
+
 for file in `find ./build/intermediates/assets -type f -name "*.??.plist"`
 do
     echo $file
