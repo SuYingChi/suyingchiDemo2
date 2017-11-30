fbf4e351fedd9d017678eef754144e375e27d8e6
rongbo.qi <rongbo.qi@ihandysoft.com>
2017-11-30 17:08:08 +0800

    add ecstasy flavor

---
+++ app/src/ecstasy/java/com/mobipioneer/lockerkeyboard/app/MyInputMethodApplication.java
@@ -0,0 +1,8 @@
+package com.mobipioneer.lockerkeyboard.app;
+
+import com.ihs.inputmethod.api.HSUIApplication;
+
+
+public class MyInputMethodApplication extends HSUIApplication {
+
+}
---
+++ app/src/ecstasy/java/com/mobipioneer/lockerkeyboard/app/MyInputMethodService.java
@@ -0,0 +1,7 @@
+package com.mobipioneer.lockerkeyboard.app;
+
+import com.ihs.inputmethod.api.HSUIInputMethodService;
+
+public class MyInputMethodService extends HSUIInputMethodService {
+
+}
