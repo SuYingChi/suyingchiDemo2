c0af0f4381c4eb903815843509ae5494f9037f90
jixiang.chen <jixiang.chen@ihandysoft.com>
2018-01-31 13:37:23 +0800

    call flash preview增加图标

--- app/src/adult/java/com.ihs.inputmethod/callflash/adapter/CallFlashAdapter.java
+++ app/src/adult/java/com.ihs.inputmethod/callflash/adapter/CallFlashAdapter.java
@@ -2,13 +2,14 @@ package com.ihs.inputmethod.callflash.adapter;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.acb.call.themes.Type;
+import com.acb.call.views.InCallActionView;
+import com.acb.call.views.ThemePreviewWindow;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
@@ -16,7 +17,6 @@ import com.ihs.feature.common.ViewUtils;
 import com.ihs.inputmethod.common.adapter.CommonAdapter;
 import com.ihs.inputmethod.uimodules.R;
 import com.ihs.inputmethod.uimodules.ui.customize.InCallThemePreviewActivity;
-import com.ihs.inputmethod.uimodules.ui.customize.adapter.LockerThemeGalleryAdapter;
 
 import static com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions.withCrossFade;
 
@@ -35,27 +35,30 @@ public class CallFlashAdapter extends CommonAdapter<Type> implements View.OnClic
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View grid = View.inflate(parent.getContext(), R.layout.item_call_flash, null);
-        ThemeViewHolder themeHolder = new ThemeViewHolder(grid);
-        themeHolder.itemView.setOnClickListener(this);
+        CallFlashViewHolder viewHolder = new CallFlashViewHolder(grid);
+        viewHolder.itemView.setOnClickListener(this);
 
         RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         int margin = parent.getResources().getDimensionPixelSize(R.dimen.theme_card_recycler_view_card_margin);
         layoutParams.setMargins(margin, margin, margin, margin);
-        themeHolder.itemView.setLayoutParams(layoutParams);
-        return themeHolder;
+        viewHolder.itemView.setLayoutParams(layoutParams);
+
+        viewHolder.inCallActionView.setAutoRun(false);
+        viewHolder.themePreviewWindow.setPreviewType(ThemePreviewWindow.PreviewType.PREVIEW);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         int themeIndex = position;
-        final ThemeViewHolder themeHolder = (ThemeViewHolder) holder;
+        final CallFlashViewHolder viewHolder = (CallFlashViewHolder) holder;
         holder.itemView.setTag(themeIndex);
         final Type themeType = dataList.get(themeIndex);
-
+        viewHolder.themePreviewWindow.updateThemeLayout(themeType);
 
         Glide.with(activity).asBitmap().apply(requestOptions)
                 .load(themeType.getPreviewImage()).transition(withCrossFade(500))
-                .into(themeHolder.themeThumbnail);
+                .into(viewHolder.callFlashImage);
     }
 
     @Override
@@ -79,27 +82,15 @@ public class CallFlashAdapter extends CommonAdapter<Type> implements View.OnClic
         }
     }
 
-    static class ThemeViewHolder extends ViewHolder {
-        ImageView themeThumbnail;
-
-        public ThemeViewHolder(View itemView) {
+    static class CallFlashViewHolder extends ViewHolder {
+        ImageView callFlashImage;
+        InCallActionView inCallActionView;
+        ThemePreviewWindow themePreviewWindow;
+        public CallFlashViewHolder(View itemView) {
             super(itemView);
-            themeThumbnail = ViewUtils.findViewById(itemView, R.id.theme_thumbnail);
-        }
-    }
-
-    public static class GridSpanSizer extends GridLayoutManager.SpanSizeLookup {
-        LockerThemeGalleryAdapter mAdapter;
-
-        public GridSpanSizer(LockerThemeGalleryAdapter adapter) {
-            super();
-            mAdapter = adapter;
-            setSpanIndexCacheEnabled(true);
-        }
-
-        @Override
-        public int getSpanSize(int position) {
-            return 1;
+            callFlashImage = ViewUtils.findViewById(itemView, R.id.call_flash_image);
+            inCallActionView = ViewUtils.findViewById(itemView, R.id.in_call_view);
+            themePreviewWindow = ViewUtils.findViewById(itemView, R.id.flash_view);
         }
     }
 }
