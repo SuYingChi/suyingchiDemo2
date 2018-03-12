030046d099930bd956b35b2d492111703e0e25b3
yingchi.su <yingchi.su@ihandysoft.com>
2018-03-12 20:54:25 +0800

    自动回复alert

--- app/src/main/java/com/ihs/inputmethod/feature/autoReply/EnableAutoResponseAlertActivity.java
+++ app/src/main/java/com/ihs/inputmethod/feature/autoReply/EnableAutoResponseAlertActivity.java
@@ -2,24 +2,23 @@ package com.ihs.inputmethod.feature.autoReply;
 
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.os.RemoteException;
 import android.support.v7.app.AlertDialog;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.ihs.app.framework.HSApplication;
 import com.ihs.app.framework.activity.HSAppCompatActivity;
+import com.ihs.devicemonitor.accessibility.HSAccessibilityService;
+import com.ihs.inputmethod.api.framework.HSInputMethod;
+import com.ihs.inputmethod.api.framework.HSInputMethodService;
 import com.ihs.inputmethod.feature.common.RoundCornerImageView;
 import com.ihs.inputmethod.uimodules.R;
 import com.ihs.keyboardutils.alerts.HSAlertDialog;
@@ -35,7 +34,9 @@ public class EnableAutoResponseAlertActivity extends HSAppCompatActivity {
 
 
     private AlertDialog dialog;
-
+    private AlertDialog permissionDialog;
+    private Handler handler = new Handler();
+    private final int START_ACCESSIBILITY_SETTINGS_REQUEST_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,9 +63,7 @@ public class EnableAutoResponseAlertActivity extends HSAppCompatActivity {
         enableAutoResponseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //开启自动回复
                 enableAutoResponse();
-                KCCommonUtils.dismissDialog(dialog);
 
             }
         });
@@ -73,8 +72,41 @@ public class EnableAutoResponseAlertActivity extends HSAppCompatActivity {
     }
 
     private void enableAutoResponse() {
-        Toast.makeText(this, "开启自动回复逻辑", Toast.LENGTH_SHORT).show();
-        KCCommonUtils.dismissDialog(dialog);
+        Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivityForResult(intent, START_ACCESSIBILITY_SETTINGS_REQUEST_CODE);
+        View permissionAlertView = View.inflate(this, R.layout.custom_enable_auto_reply_permission_alert, null);
+        GifImageView autoResponseGifImageView = (GifImageView) permissionAlertView.findViewById(R.id.enable_permission_auto_reply_animated_view);
+        Uri uri = Uri.parse("android.resource://" + HSApplication.getContext().getPackageName() + "/" + R.raw.app_theme_new_gif);
+        autoResponseGifImageView.setImageURI(uri);
+        TextView btn = (TextView) permissionAlertView.findViewById(R.id.btn_positive_single);
+        if (permissionDialog == null) {
+            permissionDialog = HSAlertDialog.build(0).setView(permissionAlertView).setCancelable(true).create();
+            permissionDialog.setCancelable(true);
+            permissionDialog.setCanceledOnTouchOutside(true);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    KCCommonUtils.dismissDialog(permissionDialog);
+                }
+            });
+        }
+        final int GUIDE_DELAY = 1000;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                KCCommonUtils.showDialog(permissionDialog);
+            }
+        }, GUIDE_DELAY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == START_ACCESSIBILITY_SETTINGS_REQUEST_CODE){
+            if(HSAccessibilityService.isAvailable()){
+                KCCommonUtils.dismissDialog(dialog);
+            }
+        }
     }
 
 }
